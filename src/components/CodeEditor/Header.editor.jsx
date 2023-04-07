2b778c69e060b35a2f51e091df19f0644d955ae1
John Aeron Sencil <senciljohnaeron@gmail.com>
2023-04-07 15:32:55 +0800

    sliced `CodeEditor.jsx` to parts for cleaner code

---
+++ src/components/CodeEditor/Header.editor.jsx
@@ -0,0 +1,31 @@
+import { IonIcon } from "@ionic/react";
+import clsx from "clsx";
+import { useCodeEditorStore } from ".";
+import FormatBtn from "./FormatBtn";
+import ToggleBtn from "./ToggleBtn";
+
+const CodeEditorHeader = ({ icon, type }) => {
+  const isMinimized = useCodeEditorStore((state) => state.isMinimized);
+
+  return (
+    <header className="flex items-center justify-between border-y border-white/10 bg-[#282c34] px-2 py-1 text-sm uppercase text-white/60">
+      <div
+        className={`flex items-center space-x-2 ${clsx({
+          "text-amber-500": type === "html",
+          "text-sky-500": type === "css",
+          "text-yellow-500": type === "js",
+        })}`}
+      >
+        <IonIcon icon={icon} className="text-base" />
+        {!isMinimized && <p className="font-semibold">{type}</p>}
+      </div>
+
+      <div className="flex items-center">
+        <FormatBtn />
+        <ToggleBtn type={type} />
+      </div>
+    </header>
+  );
+};
+
+export default CodeEditorHeader;
